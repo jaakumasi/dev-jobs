45cf41ca3250916135687d66d0a5e5baf1e5243a
jaakumasi <jeromeakumasi01@gmail.com>
2023-06-15 15:11:49 +0000

    config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#5964E0',
+      },
     },
+    screens: {
+      'lg': { 'max': '1200px' },
+      'md': { 'max': '992px' },
+      'sm': { 'max': '768px' }
+    }
   },
   plugins: [],
 }
