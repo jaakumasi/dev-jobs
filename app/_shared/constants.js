c4038d159d211ac0db4a23d7de28854d8abeb1a7
jaakumasi <jeromeakumasi01@gmail.com>
2023-06-17 11:06:41 +0000

    created shared dir

---
+++ app/_shared/constants.js
@@ -0,0 +1,8 @@
+export const SCREEN = {
+    XXXLARGE:  1600,
+    XXLARGE:  1440,
+    XLARGE:  1280,
+    LARGE: 1024,
+    MEDIUM: 768,
+    SMALL: 640
+}
---
+++ app/_shared/utilities.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const useScreenWidth = () => {
+  const [screenWidth, setScreenWidth] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    // Initial screen width
+    setScreenWidth(window.innerWidth);
+
+    // Event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return screenWidth;
+};
+
+export default useScreenWidth;
